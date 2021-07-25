c550635bd8ba2b0f660e3af2fa562921c3b398f6
lionslancer <79127091+lionslancer@users.noreply.github.com>
2021-07-25 23:33:41 +0200

    Preserve personal changes

--- config.h
+++ config.h
@@ -14,9 +14,9 @@ static const Block blocks[] = {
 	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
 	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-cpu",		10,	18},
+	{"",	"sb-moonphase",	18000,	17},
 	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-mailbox",	180,	12},
 	{"",	"sb-nettraf",	1,	16},
@@ -34,3 +34,4 @@ static char *delim = " ";
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
